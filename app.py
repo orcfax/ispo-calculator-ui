07259bda0ef77a53e5d630a25e264df794ae8318
Peter Van Garderen <peter@artefactual.com>
2023-04-03 15:50:09 -0700

    read from API and display results

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
 from flask import Flask, render_template, request
-import sqlite3
+import requests
+import json
 
 app = Flask(__name__)
-db_location = "database.sqlite"
+api_address = "https://orcfax.apexpool.info/api/v0/"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        key_value = request.form['key_value']
+        stake_key = request.form['stake_address']
 
-        db = sqlite3.connect(db_location)
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM item WHERE id=?", (key_value,),)
+        # connect to API for stake address data
+        api_url = api_address + "/get_rewards/" + stake_key
+        response = requests.get(api_url)
+        report = json.loads(response.text)
 
-        result = cursor.fetchone()
-        if result:
-            return render_template('rewards.html', result='yes')
-        else:
-            return render_template('rewards.html', result='no')
-        
+        # check for error message
+        for key in report:
+            if key == 'error':
+                error_message = report['error']
+                return render_template('error.html', stake_key=stake_key, error_message=error_message)
+
+        # format results for UI template
+        rewards_dict = {}
+        for item in report['rewards']:
+            active_stake = int(item['active_stake'])
+            epoch = int(item['epoch'])
+            rewards = float(item['rewards'])
+            rewards_dict[epoch] = [active_stake, rewards]
+
+        return render_template('rewards.html', stake_address=report['stake_address'], total_rewards=report['total_rewards'], rewards=rewards_dict)
 
     return render_template('index.html')
 
