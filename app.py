463d8c46027394469a974e774fffda12cc10549f
Peter Van Garderen <peter@artefactual.com>
2023-04-05 16:10:49 -0700

    adjust to API reformating

--- app.py
+++ app.py
@@ -36,22 +36,15 @@ def index():
             rewards_dict[epoch] = [active_stake, 
                                    epoch_rewards, epoch_bonus, truncated_epoch_rewards]
             
-        #total_ispo_stake = "{:,.0f}".format(report['live_stake'])
-
-        total_ispo_rewards = report['ispo_total_adjusted_rewards']
-        total_ispo_rewards = total_ispo_rewards.split(".")
-        total_ispo_rewards = int(total_ispo_rewards[0])
-        total_ispo_rewards_percent = truncate((total_ispo_rewards*100)/100000000,2)
-        total_ispo_rewards = "{:,.0f}".format(total_ispo_rewards)
-    
         return render_template('rewards.html',
                                latest_epoch = report['latest_epoch'],
                                total_ispo_stake = report['live_stake'],
                                total_ispo_rewards = report['ispo_total_adjusted_rewards'],
-                               total_ispo_rewards_percent = total_ispo_rewards_percent,
+                               total_ispo_rewards_percent = report['total_ispo_rewards_percent'],
                                total_ispo_bonus = report['ispo_total_bonus'],
                                stake_address=report['stake_address'], total_rewards=report['total_adjusted_rewards'],
                                rewards_share = report['rewards_percentage_from_total'],rewards=rewards_dict)
+
 
     return render_template('index.html')
 
