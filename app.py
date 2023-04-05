229a193da2151d898a76ce3fda01cf998434c32e
Peter Van Garderen <peter@artefactual.com>
2023-04-05 09:44:12 -0700

    update rewards report with extra API data

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 import json
+import math
 
 app = Flask(__name__)
 api_address = "https://orcfax.apexpool.info/api/v0/"
@@ -19,19 +20,47 @@ def index():
         for key in report:
             if key == 'error':
                 error_message = report['error']
-                return render_template('error.html', stake_address=stake_key, error_message=error_message)
+                return render_template('error.html', 
+                                       stake_address=stake_key, error_message=error_message)
 
         # format results for UI template
         rewards_dict = {}
+        cumulative_epoch_rewards = 0
         for item in report['rewards']:
             active_stake = int(item['active_stake'])
             epoch = int(item['epoch'])
-            rewards = float(item['adjusted_rewards'])
-            rewards_dict[epoch] = [active_stake, rewards]
+            epoch_rewards = float(item['base_rewards'])
+            epoch_bonus = item['bonus']
+            cumulative_epoch_rewards += float(item['adjusted_rewards'])
+            truncated_epoch_rewards = truncate(cumulative_epoch_rewards, 4)
+            rewards_dict[epoch] = [active_stake, 
+                                   epoch_rewards, epoch_bonus, truncated_epoch_rewards]
+            
+        total_ispo_stake = "{:,.0f}".format(report['live_stake'])
 
-        return render_template('rewards.html', stake_address=report['stake_address'], total_rewards=report['total_adjusted_rewards'], rewards=rewards_dict)
+        total_ispo_rewards = report['ispo_total_adjusted_rewards']
+        total_ispo_rewards = total_ispo_rewards.split(".")
+        total_ispo_rewards = int(total_ispo_rewards[0])
+        total_ispo_rewards = "{:,.0f}".format(total_ispo_rewards)
+  
+        return render_template('rewards.html',
+                               latest_epoch = report['latest_epoch'],
+                               total_ispo_stake = total_ispo_stake,
+                               total_ispo_rewards = total_ispo_rewards,
+                               total_ispo_bonus = report['ispo_total_bonus'],
+                               stake_address=report['stake_address'], total_rewards=report['total_adjusted_rewards'],
+                               rewards_share = report['rewards_percentage_from_total'],rewards=rewards_dict)
 
     return render_template('index.html')
+
+
+def truncate(number, digits) -> float:
+    # trim decimal points
+    nbDecimals = len(str(number).split('.')[1]) 
+    if nbDecimals <= digits:
+        return number
+    stepper = 10.0 ** digits
+    return math.trunc(stepper * number) / stepper
 
 if __name__ == '__main__':
     app.run(debug=True)
